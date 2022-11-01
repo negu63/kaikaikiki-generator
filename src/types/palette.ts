495ff61c42eb01e1c276e3a0f361729f639b65ad
negu63 <negu6633@gmail.com>
2022-11-01 21:04:06 +0900

    Add Palette interface

---
+++ src/types/palette.ts
@@ -0,0 +1,24 @@
+export interface Palette {
+  outline: string;
+  face: string;
+  mouthOutline: string;
+  mouth: string;
+  scleraLeft: string;
+  scleraRight: string;
+  pupilLeftTop: string;
+  pupilRightTop: string;
+  pupilLeftBottom: string;
+  pupilRightBottom: string;
+  leaf1: string;
+  leaf2: string;
+  leaf3: string;
+  leaf4: string;
+  leaf5: string;
+  leaf6: string;
+  leaf7: string;
+  leaf8: string;
+  leaf9: string;
+  leaf10: string;
+  leaf11: string;
+  leaf12: string;
+}
