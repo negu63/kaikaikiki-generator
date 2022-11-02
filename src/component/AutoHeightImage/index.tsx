906dc22619e39650ae6fcb651d37f974b0541435
negu63 <negu6633@gmail.com>
2022-11-02 17:20:05 +0900

    Add minWidth option in AuthHeightImage

--- src/component/AutoHeightImage/index.tsx
+++ src/component/AutoHeightImage/index.tsx
@@ -4,6 +4,7 @@ export default function AutoHeightImage({
   loader,
   width = 30,
   maxWidth = 400,
+  minWidth = 100,
   alt = "",
   priority = true,
   src,
@@ -11,13 +12,20 @@ export default function AutoHeightImage({
   loader?: ImageLoader;
   width?: number;
   maxWidth?: number;
+  minWidth?: number;
   alt?: string;
   priority?: boolean;
   src: string;
 }) {
   return (
     <>
-      <div style={{ maxWidth: `${maxWidth}px`, width: `${width}vw` }}>
+      <div
+        style={{
+          maxWidth: `${maxWidth}px`,
+          minWidth: `${minWidth}px`,
+          width: `${width}vw`,
+        }}
+      >
         <Image
           loader={loader}
           src={src}
