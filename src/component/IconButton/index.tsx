0e2314f5cd7660a182473cadfc488e79249050d2
negu63 <negu6633@gmail.com>
2022-11-02 17:20:34 +0900

    Add IconButton component

---
+++ src/component/IconButton/index.tsx
@@ -0,0 +1,23 @@
+import AutoHeightImage from "../AutoHeightImage";
+
+export default function IconButton({
+  alt = "",
+  iconSrc,
+  onClick,
+}: {
+  alt: string;
+  iconSrc: string;
+  onClick: Function;
+}) {
+  return (
+    <>
+      <div
+        onClick={() => {
+          onClick();
+        }}
+      >
+        <AutoHeightImage alt={alt} width={2} src={iconSrc} minWidth={32} />
+      </div>
+    </>
+  );
+}
