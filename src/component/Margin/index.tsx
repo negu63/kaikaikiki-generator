640fabe040d4806baa3df3a3269c8feae885bbb4
negu63 <negu6633@gmail.com>
2022-11-02 17:22:29 +0900

    Change Margin children type

--- src/component/Margin/index.tsx
+++ src/component/Margin/index.tsx
@@ -1,28 +1,28 @@
 export default function Margin({
-    left = 0,
-    top = 0,
-    right = 0,
-    bottom = 0,
-    children,
-  }: {
-    left?: number;
-    top?: number;
-    right?: number;
-    bottom?: number;
-    children: JSX.Element;
-  }) {
-    return (
-      <>
-        <div
-          style={{
-            marginLeft: `${left}vh`,
-            marginTop: `${top}vh`,
-            marginRight: `${right}vh`,
-            marginBottom: `${bottom}vh`,
-          }}
-        >
-          {children}
-        </div>
-      </>
-    );
-  }
+  left = 0,
+  top = 0,
+  right = 0,
+  bottom = 0,
+  children,
+}: {
+  left?: number;
+  top?: number;
+  right?: number;
+  bottom?: number;
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <div
+        style={{
+          marginLeft: `${left}vh`,
+          marginTop: `${top}vh`,
+          marginRight: `${right}vh`,
+          marginBottom: `${bottom}vh`,
+        }}
+      >
+        {children}
+      </div>
+    </>
+  );
+}
