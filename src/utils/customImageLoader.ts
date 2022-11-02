6f006e71b420231fd5caa9abb40811f92d8d2807
negu63 <negu6633@gmail.com>
2022-11-02 15:23:24 +0900

    Add CustomImageLoader util

---
+++ src/utils/customImageLoader.ts
@@ -0,0 +1,13 @@
+import { ImageLoader } from "next/image";
+import { Palette } from "../types/palette";
+
+export default function CustomImageLoader(palette: Palette): ImageLoader {
+  return ({ src }: { src: string }) => {
+    const query = Object.keys(palette).reduce(
+      (prev, cur) => prev + `${cur}=${palette[cur]}&`,
+      ""
+    );
+    return `/${src}?${query}}
+`;
+  };
+}
