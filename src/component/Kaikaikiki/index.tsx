f8eeb8741a59d11d9215a7f0844f381ade55644c
negu63 <negu6633@gmail.com>
2022-11-02 15:23:51 +0900

    Replace loader to CustomImageLoader

--- src/component/Kaikaikiki/index.tsx
+++ src/component/Kaikaikiki/index.tsx
@@ -1,19 +1,15 @@
 import AutoHeightImage from "../AutoHeightImage";
 import { useRecoilValue } from "recoil";
 import { $palette } from "../../recoil/atoms/paletteAtom";
+import CustomImageLoader from "../../utils/customImageLoader";
 
 export default function Kaikaikiki() {
   const palette = useRecoilValue($palette);
 
-  const loader = ({ src }: { src: string }) => {
-    const query = Object.keys(palette).reduce((prev, cur) => prev + `${cur}=${palette[cur]}&`, "");
-    return `/${src}?${query}}
-`;
-  };
   return (
     <>
       <AutoHeightImage
-        loader={loader}
+        loader={CustomImageLoader(palette)}
         src={"api/generated-image"}
         alt="kaikai-kiki"
         width={40}
