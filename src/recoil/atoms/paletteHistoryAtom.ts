5228a268d3079b174106f4a2de753b03a519ee7d
negu63 <negu6633@gmail.com>
2022-11-15 21:24:58 +0900

    Deduplicate default color

--- src/recoil/atoms/paletteHistoryAtom.ts
+++ src/recoil/atoms/paletteHistoryAtom.ts
@@ -5,30 +5,7 @@ import { Palette } from "../../types/palette";
 const $paletteHistory = atom<Palette[]>({
   key: "paletteHistory",
   default: [
-    {
-      outline: defaultColor.outline,
-      face: defaultColor.face,
-      mouthOutline: defaultColor.mouthOutline,
-      mouth: defaultColor.mouth,
-      scleraLeft: defaultColor.scleraLeft,
-      scleraRight: defaultColor.scleraRight,
-      pupilLeftTop: defaultColor.pupilLeftTop,
-      pupilRightTop: defaultColor.pupilRightTop,
-      pupilLeftBottom: defaultColor.pupilLeftBottom,
-      pupilRightBottom: defaultColor.pupilRightBottom,
-      leaf1: defaultColor.leaf1,
-      leaf2: defaultColor.leaf2,
-      leaf3: defaultColor.leaf3,
-      leaf4: defaultColor.leaf4,
-      leaf5: defaultColor.leaf5,
-      leaf6: defaultColor.leaf6,
-      leaf7: defaultColor.leaf7,
-      leaf8: defaultColor.leaf8,
-      leaf9: defaultColor.leaf9,
-      leaf10: defaultColor.leaf10,
-      leaf11: defaultColor.leaf11,
-      leaf12: defaultColor.leaf12,
-    },
+    defaultColor,
   ],
 });
 
